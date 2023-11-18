befbf1742571021b3a6b5e6bd5b169df6aaf7c02
Jhonwell Espanola <jhonwellespanola4@gmail.com>
2023-11-18 23:54:45 +0800

    Login Designs Completed

--- src/components/login-register/LoginCard.js
+++ src/components/login-register/LoginCard.js
@@ -13,15 +13,15 @@ export default function LoginCard({setLogSwitch, showPass}) {
            </ul>
 
            <div className='w-full input-box-con flex flex-col gap-10 items-center mt-2'>
-             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl overflow-hidden'>
-                <input type='email' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2'/>
+             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl'>
+                <input type='email' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2 rounded-xl'/>
                 <label className='absolute top-2 left-2'>Email</label>
                 <i></i>
              </div>
 
-             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl overflow-hidden'>
+             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl'>
              <img onClick={showPass} width="23" className='absolute top-3 right-3 z-20 cursor-pointer' src='./assets/Login Section/eye-slash.png' alt=''/>
-                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2'/>
+                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2 rounded-xl'/>
                 <label className='absolute top-2 left-2'>Password</label>
                 <i></i>
              </div>
--- src/components/login-register/RegisterCard.js
+++ src/components/login-register/RegisterCard.js
@@ -12,22 +12,22 @@ export default function RegisterCard({setLogSwitch, showPass}) {
            </ul>
 
            <div className='w-full input-box-con flex flex-col gap-8 items-center mt-2'>
-             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl overflow-hidden'>
-                <input type='email' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2'/>
+             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl'>
+                <input type='email' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2 rounded-xl'/>
                 <label className='absolute top-2 left-2'>Email</label>
                 <i></i>
              </div>
 
-             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl overflow-hidden'>
+             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl'>
              <img onClick={showPass} width="23" className='absolute top-3 right-3 z-20 cursor-pointer' src='./assets/Login Section/eye-slash.png' alt=''/>
-                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2'/>
+                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2 rounded-xl'/>
                 <label className='absolute top-2 left-2'>Password</label>
                 <i></i>
              </div>
 
-             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl overflow-hidden'>
+             <div className='relative border-darkBlue w-10/12 h-12 rounded-xl'>
              <img onClick={showPass} width="23" className='absolute top-3 right-3 z-20 cursor-pointer' alt='' src='./assets/Login Section/eye-slash.png'/> 
-                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2' alt=''/>
+                <input type='password' required className='absolute bg-transparent z-10 inset-0 w-full h-full px-2 rounded-xl' alt=''/>
                 <label className='absolute top-2 left-2'>Confirm Password</label>
                 <i></i>
              </div>
